28f59eaf192c13edf58591720a7565ae92a76b7c
YanDalerZ <tejaresdale@gmail.com>
2026-02-21 19:01:54 +0800

    fix bug

--- Frontend/src/interfaces.tsx
+++ Frontend/src/interfaces.tsx
@@ -1,4 +1,6 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+export const API_BASE_URL = window.location.hostname === 'localhost'
+    ? 'http://localhost:3000'
+    : 'https://baranggay-183.onrender.com';
 export default API_BASE_URL;
 
 export interface SlideData {
